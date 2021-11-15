3e27c7daf1cbdf517f85a8dca4634ab590b50889
Tommaso Laurenzi <tommaso.laurenzi@gmail.com>
2021-11-15 11:55:13 +0100

    instructions to update language servers, not meant to be run as a script

--- nvim/langserver_update.sh
+++ nvim/langserver_update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# pyright, vim-language-server, bash-language-server, 
+# pyright, vim-language-server, bash-language-server, vscode-html-language-server vscode-css-language-server vscode-json-language-server vscode-eslint-language-server
 yarn global upgrade
 
 # texlab
@@ -9,14 +9,20 @@ cargo install --git https://github.com/latex-lsp/texlab.git --locked
 go install 'github.com/mattn/efm-langserver@latest'
 
 # ccls
-# macports ccls-clang-11
+# port upgrade ccls-clang-11
+
+cd "$HOME/usr/src/ccls" || exit
+git pull
+rm -rf Release
+cmake -H. -BRelease -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=/Users/laurenzi/usr/src/clang+llvm-13.0.0-x86_64-apple-darwin
+cmake --build Release
 
 # sumneko_lua
-cd $HOME/usr/src
+cd "$HOME/usr/src" || exit
 git clone https://github.com/sumneko/lua-language-server
-cd lua-language-server
+cd lua-language-server || exit
 git submodule update --init --recursive
-cd 3rd/luamake
+cd 3rd/luamake || exit
 compile/install.sh
-cd ../..
+cd ../.. || exit
 ./3rd/luamake/luamake rebuild
