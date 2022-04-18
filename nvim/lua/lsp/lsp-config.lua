f5cac93c582c56640286caa45df88dde3cdd7eaa
Tommaso Laurenzi <tommaso.laurenzi@gmail.com>
2022-04-19 00:20:03 +0200

    all lua

--- nvim/lua/lsp/lsp-config.lua
+++ nvim/lua/lsp/lsp-config.lua
@@ -35,40 +35,14 @@ end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
-capabilities.textDocument.completion.completionItem.snippetSupport = true
-capabilities.textDocument.completion.completionItem.resolveSupport = {
-    properties = {
-        "documentation",
-        "detail",
-        "additionalTextEdits",
-    },
-}
-
-capabilities.textDocument.codeAction = {
-    dynamicRegistration = true,
-    codeActionLiteralSupport = {
-        codeActionKind = {
-            valueSet = (function()
-                local res = vim.tbl_values(vim.lsp.protocol.CodeActionKind)
-                table.sort(res)
-                return res
-            end)(),
-        },
-    },
-}
-
--- capabilities.textDocument.completion.completionItem.workDoneProgress = true
--- capabilities.window.workDoneProgress = true
+capabilities.textDocument.completion.completionItem.workDoneProgress = true
+capabilities.textDocument.codeAction.dynamicRegistration = true
 
 ---------------
 -- On Attach --
 ---------------
 
 local on_attach = function(client, bufnr)
-    local function buf_set_keymap(...)
-        vim.api.nvim_buf_set_keymap(bufnr, ...)
-    end
-
     local function buf_set_option(...)
         vim.api.nvim_buf_set_option(bufnr, ...)
     end
@@ -77,92 +51,78 @@ local on_attach = function(client, bufnr)
     local opts = {
         noremap = true,
         silent = true,
+        buffer = bufnr,
     }
-    buf_set_option("tagfunc", "v:lua.vim.lsp.tagfunc")
-    -- buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-    -- buf_set_keymap('n', '<leader>li', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-    -- buf_set_keymap('n', '<leader>la', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-    -- buf_set_keymap('x', '<leader>la', '<cmd>lua vim.lsp.buf.range_code_action()<CR>', opts)
-    -- buf_set_keymap('n', '<leader>ls', '<cmd>lua vim.lsp.buf.document_symbol()<CR>', opts)
-    -- buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-    buf_set_keymap("n", "gd", "<Cmd>Telescope lsp_definitions<CR>", opts)
-    buf_set_keymap("n", "<C-w>d", "<Cmd>split <bar> Telescope lsp_definitions<CR>", opts)
-    buf_set_keymap("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-    buf_set_keymap("n", "gr", "<cmd>Telescope lsp_references<CR>", opts)
-    buf_set_keymap("n", "<leader>lt", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
-    buf_set_keymap("n", "<leader>li", "<cmd>Telescope lsp_implementations<CR>", opts)
-    buf_set_keymap("n", "<leader>la", "<cmd>Telescope lsp_code_actions<CR>", opts)
-    buf_set_keymap(
-        "x",
-        "<leader>la",
-        "<cmd>lua require'telescope.builtin'.lsp_range_code_actions({start_line = vim.fn.line(\"'<\"), end_line = vim.fn.line(\"'>\")})<CR>",
-        opts
-    )
-    buf_set_keymap("n", "<leader>ls", "<cmd>Telescope lsp_document_symbols<CR>", opts)
-    buf_set_keymap("n", "<leader>lS", "<cmd>Telescope lsp_dynamic_workspace_symbols<CR>", opts)
-    buf_set_keymap(
-        "n",
-        "<leader>lwl",
-        '<cmd>lua print(table.concat(vim.lsp.buf.list_workspace_folders(), ", "))<CR>',
-        opts
-    )
-    buf_set_keymap("n", "<leader>lwa", '<cmd>lua require"lsp.utilities".addWsFolder()<CR>', opts)
-    buf_set_keymap("n", "<leader>lwr", '<cmd>lua require"lsp.utilities".removeWsFolder()<CR>', opts)
-    buf_set_keymap("n", "<leader>lr", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
-    buf_set_keymap(
-        "n",
-        "<leader>ld",
-        '<cmd>lua vim.diagnostic.open_float(nil, {scope ="line", border = "rounded"})<CR>',
-        opts
-    )
-    buf_set_keymap("n", "<leader>lq", "<cmd>lua vim.diagnostic.setqflist()<CR>", opts)
-    buf_set_keymap("n", "K", "<Cmd>lua vim.lsp.buf.hover()<CR>", opts)
-    buf_set_keymap("i", "<C-q>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
-    buf_set_keymap("n", "<C-q>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
-    buf_set_keymap("n", "[d", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>", opts)
-    buf_set_keymap("n", "]d", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", opts)
-    -- buf_set_keymap('n', '<leader>lg', '<cmd>lua peek_definition()<CR>', opts) -- treesitter does it better atm
+    vim.api.nvim_buf_set_option(bufnr, "tagfunc", "v:lua.vim.lsp.tagfunc")
+    -- vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    -- vim.keymap.set('n', '<leader>li', vim.lsp.buf.implementation, opts)
+    -- vim.keymap.set('n', '<leader>la', vim.lsp.buf.code_action, opts)
+    vim.keymap.set("x", "<leader>la", vim.lsp.buf.range_code_action, opts)
+    -- vim.keymap.set('n', '<leader>ls', vim.lsp.buf.document_symbol, opts)
+    -- vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    -- vim.keymap.set("n", "<leader>lt", vim.lsp.buf.type_definition, opts)
+    vim.keymap.set("n", "gd", require("telescope.builtin").lsp_definitions, opts)
+    vim.keymap.set("n", "<C-w>d", "<Cmd>split <bar> Telescope lsp_definitions<CR>", opts)
+    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+    vim.keymap.set("n", "gr", require("telescope.builtin").lsp_references, opts)
+    vim.keymap.set("n", "<leader>lt", require("telescope.builtin").lsp_type_definitions, opts)
+    vim.keymap.set("n", "<leader>li", require("telescope.builtin").lsp_implementations, opts)
+    vim.keymap.set("n", "<leader>la", require("telescope.builtin").lsp_code_actions, opts)
+    -- vim.keymap.set("x", "<leader>la", require("telescope.builtin").lsp_range_code_actions, opts)
+    vim.keymap.set("n", "<leader>ls", require("telescope.builtin").lsp_document_symbols, opts)
+    vim.keymap.set("n", "<leader>lS", require("telescope.builtin").lsp_dynamic_workspace_symbols, opts)
+    vim.keymap.set("n", "<leader>lwl", function()
+        vim.pretty_print(vim.lsp.buf.list_workspace_folders())
+    end, opts)
+    vim.keymap.set("n", "<leader>lwa", require("lsp.utilities").addWsFolder, opts)
+    vim.keymap.set("n", "<leader>lwr", require("lsp.utilities").removeWsFolder, opts)
+    vim.keymap.set("n", "<leader>lr", vim.lsp.buf.rename, opts)
+    vim.keymap.set("n", "<leader>ld", function()
+        vim.diagnostic.open_float(nil, { scope = "line", border = borders })
+    end, opts)
+    vim.keymap.set("n", "<leader>lq", vim.diagnostic.setqflist, opts)
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+    vim.keymap.set({ "n", "i" }, "<C-q>", vim.lsp.buf.signature_help, opts)
+    vim.keymap.set("n", "[d", vim.lsp.diagnostic.goto_prev, opts)
+    vim.keymap.set("n", "]d", vim.lsp.diagnostic.goto_next, opts)
+    -- buf_set_keymap('n', '<leader>lg', peek_definition, opts) -- treesitter does it better atm
 
     if client.resolved_capabilities.document_formatting then
-        buf_set_keymap("n", "<leader>lf", "<cmd>lua vim.lsp.buf.formatting_seq_sync()<CR>", opts)
-        vim.cmd([[command! -buffer LspFormat lua vim.lsp.buf.formatting_seq_sync()]])
-    elseif client.resolved_capabilities.document_range_formatting then
-        buf_set_keymap("x", "<leader>lf", "<cmd>lua vim.lsp.buf.range_formatting()<CR>", opts)
-        vim.cmd([[command! -buffer -range LspRangeFormat lua vim.lsp.buf.range_formatting()]])
+        vim.keymap.set("n", "<leader>lf", vim.lsp.buf.formatting_seq_sync, opts)
+        vim.api.nvim_buf_create_user_command(bufnr, "LspFormat", vim.lsp.buf.formatting_seq_sync, {})
     end
 
-    vim.cmd([[
-        augroup lsp_echo_diagnostics
-            autocmd! * <buffer>
-            autocmd CursorHold <buffer> lua require'lsp.utilities'.echo_cursor_diagnostic()
-            "autocmd CursorHold <buffer> lua vim.diagnostic.open_float(nil, {scope = "cursor", border = "rounded", focusable = false})
-            autocmd CursorMoved <buffer> echo ""
-        augroup END
-    ]])
-
-    if client.resolved_capabilities.signature_help then
-        vim.cmd([[
-            augroup lsp_signature_help
-                autocmd! * <buffer>
-                "autocmd CursorHoldI <buffer> lua vim.lsp.buf.signature_help({focusable = false})
-            augroup END
-        ]])
+    if client.resolved_capabilities.document_range_formatting then
+        vim.keymap.set("x", "<leader>lf", vim.lsp.buf.range_formatting, opts)
+        vim.api.nvim_buf_create_user_command(bufnr, "LspRangeFormat", vim.lsp.buf.formatting_seq_sync, { range = true })
     end
+
+    local lsp_diag_au_id = vim.api.nvim_create_augroup("LSP_diagnostics", { clear = true })
+    vim.api.nvim_create_autocmd(
+        "CursorHold",
+        { callback = require("lsp.utilities").echo_cursor_diagnostic, group = lsp_diag_au_id, buffer = bufnr }
+    )
+    vim.api.nvim_create_autocmd("CursorMoved", { command = 'echo ""', group = lsp_diag_au_id, buffer = bufnr })
+
+    -- if client.resolved_capabilities.signature_help then
+    --     local lsp_signature_help_au_id = vim.api.nvim_create_augroup("LSP_signature_help", { clear = true })
+    --     vim.api.nvim_create_autocmd(
+    --         { "CursorHoldI" },
+    --         { callback = function() vim.lsp.buf.signature_help({ focusable = false }) end, group = lsp_signature_help_au_id, buffer = bufnr }
+    --     )
+    -- end
 
     if client.resolved_capabilities.document_highlight then
-        vim.cmd([[
-            augroup lsp_document_highlight
-                autocmd! * <buffer>
-                autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
-                "autocmd CursorHoldI <buffer> lua vim.lsp.buf.document_highlight()
-                autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
-            augroup END
-        ]])
+        local lsp_references_au_id = vim.api.nvim_create_augroup("LSP_references", { clear = true })
+        vim.api.nvim_create_autocmd(
+            "CursorHold",
+            { callback = vim.lsp.buf.document_highlight, buffer = bufnr, group = lsp_references_au_id }
+        )
+        vim.api.nvim_create_autocmd(
+            "CursorMoved",
+            { callback = vim.lsp.buf.clear_references, buffer = bufnr, group = lsp_references_au_id }
+        )
     end
-
-    -- if client.name == "pyright" then
-    --     vim.api.nvim_buf_add_user_command(bufnr, "PythonInterpreter", require'lsp.utilities'.change_python_interpreter, {nargs = 1, complete = require'lsp.utilities'.get_python_interpreters})
-    -- end
 end
 
 local function make_config(server_name)
@@ -202,10 +162,6 @@ for _, server in ipairs(servers) do
     local config = make_config(server)
     lspconfig[server].setup(config)
 end
-
---------------
--- Commands --
---------------
 
 M = {}
 M.on_attach = on_attach
