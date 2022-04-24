b7cef70f24edf41e1364af69dca6cbde23d05df9
Tommaso Laurenzi <tommaso.laurenzi@gmail.com>
2022-04-24 02:10:38 +0200

    add win_ui_input

---
+++ nvim/lua/win_ui_input.lua
@@ -0,0 +1,61 @@
+local function wininput(opts, on_confirm, win_opts)
+    -- {listed, scratch}
+    local buf = vim.api.nvim_create_buf(false, false)
+    vim.bo[buf].buftype = "prompt"
+    vim.bo[buf].bufhidden = "wipe"
+
+    local prompt = opts.prompt or ""
+    local default_text = opts.default or ""
+
+    local deferred_callback = function(input)
+        vim.defer_fn(function()
+            on_confirm(input)
+        end, 10)
+    end
+
+    vim.fn.prompt_setcallback(buf, deferred_callback)
+    vim.fn.prompt_setprompt(buf, prompt)
+
+    vim.keymap.set({ "i", "n" }, "<CR>", "<CR><Esc>:close!<CR>:stopinsert<CR>", { silent = true, buffer = buf })
+    vim.keymap.set("n", "<esc>", "<cmd>close!<CR>", { silent = true, buffer = buf })
+    vim.keymap.set("n", "q", "<cmd>close!<CR>", { silent = true, buffer = buf })
+
+    local default_win_opts = {
+        relative = "editor",
+        row = vim.o.lines / 2 - 1,
+        col = vim.o.columns / 2 - 25,
+        width = 50,
+        height = 1,
+        focusable = true,
+        style = "minimal",
+        border = "none",
+    }
+
+    win_opts = vim.tbl_deep_extend("force", default_win_opts, win_opts)
+    win_opts.width = #default_text + #prompt + 5 < win_opts.width and win_opts.width or #default_text + #prompt + 5
+
+    local win = vim.api.nvim_open_win(buf, true, win_opts)
+    vim.api.nvim_win_set_option(win, "winhighlight", "Search:None")
+
+    vim.cmd("startinsert!")
+    vim.defer_fn(function()
+        vim.api.nvim_buf_set_text(buf, 0, #prompt, 0, #prompt, { default_text })
+        vim.cmd("startinsert!")
+    end, 10)
+end
+
+
+-- wininput({
+--     prompt = "Enter a number: ",
+--     default = "42",
+--     },
+--     function(input)
+--         print("input: " .. input)
+--     end,
+--     {}
+-- )
+
+vim.api.nvim_create_namespace('winui')
+vim.ui.input = function(opts, on_confirm)
+    wininput(opts, on_confirm, { border = require'lsp.lsp-config'.borders, relative = "cursor", row = 1, col = 0, width = 0 })
+end
