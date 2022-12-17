1b16b4c66b9f6d6f846d90b8c2fc5a4c074f2594
Tommaso Laurenzi <tommaso.laurenzi@gmail.com>
2022-12-17 15:28:38 +0100

    update

---
+++ nvim/lua/searchyank.lua
@@ -0,0 +1,18 @@
+-- Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+-- sed diam nonumy eirmod tempor invidunt ut
+-- labore et dolore magna aliquyam erat, sed diam
+-- voluptua. At vero eos et accusam et justo
+-- duo dolores et ea rebum. Stet clita kasd
+-- gubergren, no sea takimata sanctus est Lorem ipsum
+-- dolor sit amet.
+
+vim.api.nvim_create_user_command("SearchYank", function(args)
+    vim.fn.setreg(args.reg, "")
+    local line1 = args.line1 or 1
+    local line2 = args.line2 or vim.fn.line('$')
+    vim.api.nvim_cmd({
+        cmd = "substitute",
+        args = { string.format([[//\=setreg('%s', submatch(0), 'al')/n]], args.reg) },
+        range = { line1, line2 },
+    }, {})
+end, { range = true, register = true })
