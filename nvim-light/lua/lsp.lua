865e22d3a888f2aace91d79d650e190bd1a0259c
Tommaso Laurenzi <tommaso.laurenzi@gmail.com>
2025-01-17 09:25:04 +0100

    update

--- nvim-light/lua/lsp.lua
+++ nvim-light/lua/lsp.lua
@@ -1,6 +1,7 @@
 vim.api.nvim_create_autocmd('LspAttach', {
     callback = function(args)
         local client = vim.lsp.get_client_by_id(args.data.client_id)
+        print(client.name, "attached to buf", args.buf, vim.api.nvim_buf_get_name(args.buf))
 
         vim.keymap.set("n", "gd", vim.lsp.buf.definition)
         vim.keymap.set("n", "gD", vim.lsp.buf.declaration)
@@ -10,11 +11,11 @@ vim.api.nvim_create_autocmd('LspAttach', {
             vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
         end
 
-        local augrp = vim.api.nvim_create_augroup("document_highlight", { clear = true })
+        local augrp = vim.api.nvim_create_augroup("document_highlight_" .. args.buf, { clear = true })
         if client:supports_method('textDocument/documentHighlight') then
             vim.api.nvim_create_autocmd("CursorHold", {
                 callback = function() vim.lsp.buf.document_highlight() end,
-                buffer = bufnr,
+                buffer = args.buf,
                 group = augrp,
             })
             vim.api.nvim_create_autocmd("CursorMoved", {
